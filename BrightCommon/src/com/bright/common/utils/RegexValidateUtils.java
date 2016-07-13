0c6e8c37d9376e3b4882f98bee020c5a3440e650
公司电脑 <yuhaiyang@company.com>
2016-07-13 17:43:42 +0800

    add to Jcenter

---
+++ BrightCommon/src/com/bright/common/utils/RegexValidateUtils.java
@@ -0,0 +1,68 @@
+/**
+ * Copyright (C) 2016 The yuhaiyang Android Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.bright.common.utils;
+
+import android.text.TextUtils;
+import android.util.Log;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * 使用正则表达式验证输入格式
+ */
+public class RegexValidateUtils {
+    private static final String TAG = "RegexValidateUtils";
+
+    /**
+     * 验证邮箱
+     */
+    public static boolean checkEmail(String email) {
+        Log.d(TAG, "checkEmail:email = " + email);
+        if (TextUtils.isEmpty(email) || !email.contains("@")) {
+            return false;
+        }
+
+        if (email.split("@").length != 2) {
+            return false;
+        }
+
+        try {
+            String check = "^(\\w)+(\\.\\w+)*@(\\w)+((\\.\\w{2,3}){1,3})$";
+            Pattern regex = Pattern.compile(check);
+            Matcher matcher = regex.matcher(email);
+            return matcher.matches();
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    /**
+     * 验证手机号码
+     */
+    public static boolean checkMobileNumber(String number) {
+        boolean flag = false;
+        try {
+            Pattern regex = Pattern.compile("^((1[3-9][0-9])\\d{8})$");
+            Matcher matcher = regex.matcher(number);
+            flag = matcher.matches();
+        } catch (Exception e) {
+            flag = false;
+        }
+        return flag;
+    }
+}
