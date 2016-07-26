b804a303de1eae514c7babcacf4393f379b75584
yuhaiyang <yuhaiyang@windows.com>
2016-07-26 23:00:42 +0800

    fix a bug

--- BrightCommon/src/com/bright/common/widget/dialog/ShowPhotoDialog.java
+++ BrightCommon/src/com/bright/common/widget/dialog/ShowPhotoDialog.java
@@ -1,12 +1,12 @@
 /**
  * Copyright (C) 2016 yuhaiyang android source project
- * <p>
+ * <p/>
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * <p>
+ * <p/>
  * http://www.apache.org/licenses/LICENSE-2.0
- * <p>
+ * <p/>
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -21,6 +21,7 @@ import android.content.Context;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.view.ViewPager;
+import android.text.TextUtils;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
@@ -70,7 +71,7 @@ public class ShowPhotoDialog extends Dialog {
     }
 
     public void setData(String url) {
-        if (mUrls == null) {
+        if (TextUtils.isEmpty(url)) {
             mUrls = new ArrayList<>();
         } else {
             mUrls.clear();
@@ -79,7 +80,7 @@ public class ShowPhotoDialog extends Dialog {
     }
 
     public void setData(List<String> urls) {
-        if (mUrls == null) {
+        if (urls == null) {
             mUrls = new ArrayList<>();
         } else {
             mUrls = urls;
