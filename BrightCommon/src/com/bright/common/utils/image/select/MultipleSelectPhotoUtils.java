18dd7656b205db8d0d8c0fdee64ff31b24d9f7a8
公司电脑 <yuhaiyang@company.com>
2016-08-09 16:13:23 +0800

    update

--- BrightCommon/src/com/bright/common/utils/image/select/MultipleSelectPhotoUtils.java
+++ BrightCommon/src/com/bright/common/utils/image/select/MultipleSelectPhotoUtils.java
@@ -1,12 +1,12 @@
 /**
  * Copyright (C) 2016 The yuhaiyang Android Source Project
- * <p>
+ * <p/>
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * <p>
+ * <p/>
  * http://www.apache.org/licenses/LICENSE-2.0
- * <p>
+ * <p/>
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -86,7 +86,7 @@ public class MultipleSelectPhotoUtils extends SelectPhotoUtils {
         Intent intent;
         switch (which) {
             case SELECT_PHOTO_CAMERA:
-                mCameraFileUri = Uri.parse(ImageUtils.generateRandomPhotoName(mContext));
+                mCameraFileUri = Uri.fromFile(ImageUtils.generateRandomPhotoFile(mContext));
                 intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
                 intent.putExtra(MediaStore.EXTRA_OUTPUT, mCameraFileUri);
                 mContext.startActivityForResult(intent, Request.REQUEST_MULTI_CAMERA);
