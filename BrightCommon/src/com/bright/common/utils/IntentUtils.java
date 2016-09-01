bbdaeeab62cdf61c1b876ab7373d7ddb25b04e22
公司电脑 <yuhaiyang@company.com>
2016-09-01 17:21:12 +0800

    1. 添加.gitignore 不知道怎么没有了 2. IntentUtils 中把参数的前后空格去调了 3. Http请求的onError判断Activity是否已经销毁

--- BrightCommon/src/com/bright/common/utils/IntentUtils.java
+++ BrightCommon/src/com/bright/common/utils/IntentUtils.java
@@ -1,12 +1,12 @@
 /**
  * Copyright (C) 2016 The yuhaiyang Android Source Project
- *
+ * <p/>
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
+ * <p/>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p/>
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -23,6 +23,7 @@ import android.net.Uri;
 import android.text.TextUtils;
 import android.widget.Toast;
 
+import com.bright.common.R;
 import com.bright.common.widget.YToast;
 
 /**
@@ -34,9 +35,14 @@ public class IntentUtils {
      * 跳转拨号盘界面
      */
     public static void goToDialpad(Context context, String number) {
-        Intent intent = new Intent(Intent.ACTION_DIAL, Uri.parse("tel:" + number));
-        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-        context.startActivity(intent);
+        try {
+            Intent intent = new Intent(Intent.ACTION_DIAL, Uri.parse("tel:" + number.trim()));
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            context.startActivity(intent);
+        } catch (ActivityNotFoundException e) {
+            YToast.makeText(context, R.string.exception_intent_open, Toast.LENGTH_SHORT).show();
+        }
+
     }
 
     /**
@@ -56,12 +62,12 @@ public class IntentUtils {
         }
 
         try {
-            Uri uri = Uri.parse("market://details?id=" + packageName);
+            Uri uri = Uri.parse("market://details?id=" + packageName.trim());
             Intent intent = new Intent(Intent.ACTION_VIEW, uri);
             intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
             context.startActivity(intent);
         } catch (ActivityNotFoundException e) {
-            YToast.makeText(context, "Couldn't launch the market !", Toast.LENGTH_SHORT).show();
+            YToast.makeText(context, R.string.exception_intent_open, Toast.LENGTH_SHORT).show();
         }
     }
 
@@ -73,10 +79,10 @@ public class IntentUtils {
             return;
         }
         try {
-            Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+            Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url.trim()));
             context.startActivity(intent);
         } catch (ActivityNotFoundException e) {
-            YToast.makeText(context, "没有找到相关应用来打开：" + url, Toast.LENGTH_SHORT).show();
+            YToast.makeText(context, R.string.exception_intent_open, Toast.LENGTH_SHORT).show();
         }
     }
 
@@ -85,10 +91,10 @@ public class IntentUtils {
      */
     public static void openApp(Context context, String appPackageName) {
         try {
-            Intent intent = context.getPackageManager().getLaunchIntentForPackage(appPackageName);
+            Intent intent = context.getPackageManager().getLaunchIntentForPackage(appPackageName.trim());
             context.startActivity(intent);
         } catch (Exception e) {
-
+            YToast.makeText(context, R.string.exception_intent_open, Toast.LENGTH_SHORT).show();
         }
     }
 }
--- BrightCommon/src/com/bright/common/utils/http/okhttp/callback/CallBack.java
+++ BrightCommon/src/com/bright/common/utils/http/okhttp/callback/CallBack.java
@@ -1,12 +1,12 @@
 /**
  * Copyright (C) 2016 The yuhaiyang Android Source Project
- * <p>
+ * <p/>
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * <p>
+ * <p/>
  * http://www.apache.org/licenses/LICENSE-2.0
- * <p>
+ * <p/>
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -177,6 +177,14 @@ public abstract class CallBack<T> {
 
         if (e != null) {
             DEBUG.d(mLogTag, StringUtils.plusString(id, " EXCEPTION  = " + e.toString()));
+        }
+
+        if (mContext != null && mContext instanceof Activity) {
+            Activity activity = (Activity) mContext;
+            if (activity.isFinishing()) {
+                DEBUG.d(mLogTag, StringUtils.plusString(id, " on Error activity is finishing to do nothing "));
+                return;
+            }
         }
 
         OkHttpUtils.getInstance().getPlatform().execute(new Runnable() {
