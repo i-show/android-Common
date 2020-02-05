0fd2643f5b101e05007753ee005e12db3bfd433e
yuhaiyang <yuhaiyang@cityshield.net>
2020-02-05 17:49:16 +0800

    增加了一个Delay 方法

--- common/src/main/java/com/ishow/common/extensions/AnyExt.kt
+++ common/src/main/java/com/ishow/common/extensions/AnyExt.kt
@@ -1,10 +1,10 @@
 package com.ishow.common.extensions
 
 import android.os.Looper
-import com.google.gson.Gson
 import com.ishow.common.utils.JsonUtils
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 
 
@@ -19,5 +19,21 @@ fun isMainThread(): Boolean = Looper.myLooper() == Looper.getMainLooper()
  * 通过协程  在主线程上运行
  */
 fun mainThread(block: () -> Unit) = GlobalScope.launch(Dispatchers.Main) {
+    block()
+}
+
+/**
+ * Delay
+ */
+fun delay(long: Long, block: () -> Unit) = GlobalScope.launch(Dispatchers.Main) {
+    delay(long)
+    block()
+}
+
+/**
+ * Delay
+ */
+fun delayInThread(long: Long, block: () -> Unit) = GlobalScope.launch {
+    delay(long)
     block()
 }
